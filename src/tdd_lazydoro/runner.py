cb919702f491382d9fc7a2c513c5c1885a3f83f9
Romilly Cocking <romilly.cocking@gmail.com>
2022-03-29 15:45:14 +0100

    Problems with the distance sensor.

--- src/tdd_lazydoro/runner.py
+++ src/tdd_lazydoro/runner.py
@@ -6,6 +6,8 @@ from time import sleep
 from tdd_lazydoro.blinkt_display import BlinktDisplay
 from tdd_lazydoro.pomodoro import Pomodoro
 
+i2c = busio.I2C(board.SCL, board.SDA)
+vl53 = adafruit_vl53l0x.VL53L0X(i2c)
 
 class Alarm:
     def __init__(self, pomodoro: Pomodoro):
@@ -45,8 +47,6 @@ class Runner:
         self.alarm = alarm
         self.watcher = watcher
         self.snooze_time = 1
-        self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.vl53 = adafruit_vl53l0x.VL53L0X(self.i2c)
 
     def run(self, speed=1):
         if speed == 1:
@@ -55,7 +55,7 @@ class Runner:
             self.snooze_time = 1 / speed
         while True:
             self.alarm.tick()
-            self.watcher.range(self.vl53.range)
+            self.watcher.range(vl53.range)
             sleep(self.snooze_time)
 
 
