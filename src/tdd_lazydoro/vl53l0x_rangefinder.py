b2462cea9dc54d7cc2f0d8e54b86d2440d0d8514
Romilly Cocking <romilly.cocking@gmail.com>
2022-04-02 09:18:20 +0100

    Fixed bug in rangefinder code

--- src/tdd_lazydoro/vl53l0x_rangefinder.py
+++ src/tdd_lazydoro/vl53l0x_rangefinder.py
@@ -11,7 +11,7 @@ class VL53L0XRangeFinder(RangeFinder):
         self.vl53 = adafruit_vl53l0x.VL53L0X(self.i2c)
 
     def range(self) -> int:
-        tof_range = self.range()
+        tof_range = self.vl53.range
         if tof_range == 0:  # sometimes get this when nothing is in range
             tof_range = 8191
         return tof_range
