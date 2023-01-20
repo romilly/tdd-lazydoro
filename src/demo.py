a78786a4a9a23d14a87731091376ffa17a224796
Romilly Cocking <romilly.cocking@gmail.com>
2023-01-20 16:46:56 +0000

    Hack to run on Pi without mqtt

--- src/demo.py
+++ src/demo.py
@@ -5,7 +5,14 @@ from tdd_lazydoro.raspi.build import build
 """
 Run fast for demo purposes
 """
-runner = build(ticks_per_minute=5, speed=5)
+
+
+class PrintingMessenger:
+    def send(self, message):
+        print(message)
+
+
+runner = build(ticks_per_minute=5, speed=5, messenger=PrintingMessenger())
 runner.run()
 
 
