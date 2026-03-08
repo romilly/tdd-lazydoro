a18e52d5c9e1e8d546799dd970d892647f1e33f4
Romilly Cocking <romilly.cocking@gmail.com>
2026-03-08 17:11:06 +0000

    Switched to edgy-hermes as broker

--- src/runner.py
+++ src/runner.py
@@ -3,7 +3,7 @@
 from tdd_lazydoro.raspi.build import build
 from tdd_lazydoro.raspi.mqtt import MQTTMessenger
 
-runner = build(messenger=MQTTMessenger('watcher'))
+runner = build(messenger=MQTTMessenger('edgy-hermes'))
 runner.run()
 
 
